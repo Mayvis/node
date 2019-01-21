4cb4f2491d246430510ef27d304a91a8d1b90b42
= <=>
2019-01-21 14:39:55 +0800

    Add 404 page not found page

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extends: false}));
 
 app.use(adminRoutes);
-
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found.</h1>');
+});
 
 app.listen(3000);
 
