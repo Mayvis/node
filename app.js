91511e53539534ae2dce90b2e507429d95c9d65c
= <=>
2019-01-22 23:41:25 +0800

    Css file static serving

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -7,13 +9,13 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extends: false}));
+app.use(express.static(path.join(__dirname, 'public'))); // css file to be static
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes); // Only route which begin with /admin will trigger adminRoutes.
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found.</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
-
--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,17 @@
+const path = require('path');
+const rootDir = require('../utility/path');
 const express = require('express');
 
 const router = express.Router();
 
-router.use('/add-product', (req, res, next) => {
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
     console.log('In another middleware!');
-    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add product</button></form>');
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post('/product', (req, res, next) => {
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
