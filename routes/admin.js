c6798452cdf757c5abf5085abdd9c6a654bedc4d
= <=>
2019-01-28 11:19:59 +0800

    Add a layout folder to reuse

--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,9 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', {pageTitle: 'Add product'});
+
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
