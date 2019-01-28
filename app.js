60788957a83d0990f17e41e0d0e115824984a752
= <=>
2019-01-28 11:44:18 +0800

    Filter render condition

--- app.js
+++ app.js
@@ -18,8 +18,9 @@ app.use('/admin', adminData.routes); // Only route which begin with /admin will 
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render(404);
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+
     // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.listen(3000);
+app.listen(3001);
